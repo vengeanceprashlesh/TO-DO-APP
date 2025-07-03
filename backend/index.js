8b8ea27098ca0015ab65154a8fbd9d77616077f7
prashlesh <iamprashlesh@gmail.com>
2025-07-03 11:02:33 +0530

    added features in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@
 // with express.json() middlewares
 
 const express = require('express');
+const { createTodo, UpdateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post("/todos", function(req,res){
-
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "you sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.get("/todos", function(req,res){
@@ -16,5 +24,12 @@ app.get("/todos", function(req,res){
 
 
 app.put("/completed", function(req,res){
-    
+    const updatePayload = req.body
+    const parsedPayload = UpdateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 })
